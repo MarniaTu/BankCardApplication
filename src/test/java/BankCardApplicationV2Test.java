51d4b07d4047b079595f23e1b349efc6f9dad4a3
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2024-02-07 19:50:10 +0300

    add tests for fields validation in BankCardApplicationV2Test

---
+++ src/test/java/BankCardApplicationV2Test.java
@@ -0,0 +1,76 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankCardApplicationV2Test {
+    @Test
+    public void shouldTestAllBlankFields() {
+
+        open("http://localhost:9999/");
+
+        SelenideElement form = $("form");
+
+        form.$("button").click();
+        $(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+
+    }
+
+    @Test
+    public void shouldTestNameFieldLatinLetters() {
+
+        open("http://localhost:9999/");
+
+        SelenideElement form = $("form");
+
+        form.$("[data-test-id=name] input").setValue("Ivanov Semen");
+        form.$("button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    public void shouldTestNameFieldSlash() {
+
+        open("http://localhost:9999/");
+
+        SelenideElement form = $("form");
+
+        form.$("[data-test-id=name] input").setValue("Иванов/Петров");
+        form.$("button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    public void shouldTestPhoneFieldWithoutPlus() {
+
+        open("http://localhost:9999/");
+
+        SelenideElement form = $("form");
+
+        form.$("[data-test-id=name] input").setValue("Иванов-Петров Семен Васильевич");
+        form.$("[data-test-id=phone] input").setValue("89061236789");
+        form.$("button").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+    @Test
+    public void shouldTestWithoutAgreement() {
+
+        open("http://localhost:9999/");
+
+        SelenideElement form = $("form");
+
+        form.$("[data-test-id=name] input").setValue("Иванов-Петров Семен Васильевич");
+        form.$("[data-test-id=phone] input").setValue("+79061236789");
+        form.$("button").click();
+        $("[data-test-id=agreement]").shouldHave(attribute("class", "checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid"));
+
+    }
+}
